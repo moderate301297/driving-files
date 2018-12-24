53c600e3bfd642f5f80fc60633072de7a17a54d2
Ubuntu <ubuntu@ip-172-31-40-239.ap-southeast-1.compute.internal>
2018-12-24 21:38:31 +0000

    update

---
+++ driving-files/scripts/chaincode-cli.sh
@@ -0,0 +1,193 @@
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/orgorderer/msp/tlscacerts/tlsca.orgorderer-cert.pem
+
+echo "Channel name : "$CHANNEL_NAME
+
+CC_SRC_PATH1="github.com/hyperledger/fabric/peer/chaincode/go01/chaincode_result"
+CC_SRC_PATH2="github.com/hyperledger/fabric/peer/chaincode/go01/chaincode_certificate"
+
+
+ORDERER0_IP=orderer0.orgorderer
+ORDERER1_IP=orderer1.orgorderer
+
+setGlobals () {
+    PEER=$1
+    ORG=$2
+
+	if [ $ORG -eq 1 ] ; then
+        CORE_PEER_LOCALMSPID="Org1MSP"
+        CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1/peers/peer0.org1/tls/ca.crt
+        CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1/users/Admin@org1/msp
+        if [ $PEER -eq 0 ]; then
+            CORE_PEER_ID=peer0.org1
+            CORE_PEER_ADDRESS=peer0.org1:7051
+            CORE_PEER_CHAINCODELISTENADDRESS=peer0.org1:7052
+        else
+            CORE_PEER_ADDRESS=peer1.org1:7051
+            CORE_PEER_ID=peer1.org1
+            CORE_PEER_CHAINCODELISTENADDRESS=peer1.org1:7052
+        fi
+    elif [ $ORG -eq 2 ] ; then
+        CORE_PEER_LOCALMSPID="Org2MSP"
+        CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2/peers/peer0.org2/tls/ca.crt
+        CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2/users/Admin@org2/msp
+        if [ $PEER -eq 0 ]; then
+            CORE_PEER_ADDRESS=peer0.org2:7051
+            CORE_PEER_ID=peer0.org2
+            CORE_PEER_CHAINCODELISTENADDRESS=peer0.org2:7052
+        else
+            CORE_PEER_ADDRESS=peer1.org2:7051
+            CORE_PEER_ID=peer1.org2
+            CORE_PEER_CHAINCODELISTENADDRESS=peer1.org2:7052
+        fi
+    fi
+
+	env |grep CORE
+}
+
+installChaincode () {
+	PEER=$1
+	ORG=$2
+	setGlobals $PEER $ORG
+	VERSION=$3
+        set -x
+	peer chaincode install -n $CHAINCODE_NAME1 -v ${VERSION} -l ${LANGUAGE} -p ${CC_SRC_PATH1} >&log1.txt
+	peer chaincode install -n $CHAINCODE_NAME2 -v ${VERSION} -l ${LANGUAGE} -p ${CC_SRC_PATH2} >&log2.txt
+    
+    res=$?
+        set +x
+	cat log1.txt
+    cat log2.txt
+	verifyResult $res "Chaincode installation on peer${PEER}.org${ORG} has Failed"
+	echo "===================== Chaincode is installed on peer${PEER}.org${ORG} ===================== "
+	echo
+}
+
+instantiateChaincode () {
+	PEER=$1
+	ORG=$2
+	setGlobals $PEER $ORG
+	VERSION=$3
+
+	local ORDERER=
+    if [ $ORG -eq 1 ]; then
+        ORDERER=$ORDERER0_IP:7050
+    elif [ $ORG -eq 2 ]; then
+        ORDERER=$ORDERER1_IP:7050
+    fi
+
+	# while 'peer chaincode' command can get the orderer endpoint from the peer (if join was successful),
+	# lets supply it directly as we know it using the "-o" option
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer chaincode instantiate -o $ORDERER -C $CHANNEL_NAME -n $CHAINCODE_NAME1 -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')" >&log1.txt
+        peer chaincode instantiate -o $ORDERER -C $CHANNEL_NAME -n $CHAINCODE_NAME2 -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')" >&log2.txt
+		res=$?
+                set +x
+	else
+                set -x
+		peer chaincode instantiate -o $ORDERER --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME1 -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')" >&log1.txt
+        peer chaincode instantiate -o $ORDERER --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME2 -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')" >&log2.txt
+		res=$?
+                set +x
+	fi
+	cat log1.txt
+    cat log2.txt
+	verifyResult $res "Chaincode instantiation on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' failed"
+	echo "===================== Chaincode Instantiation on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' ===================== "
+	echo
+}
+
+
+upgradeChaincode () {
+	PEER=$1
+    ORG=$2
+    setGlobals $PEER $ORG
+    VERSION=$3
+
+	local ORDERER=
+    if [ $ORG -eq 1 ]; then
+        ORDERER=$ORDERER0_IP:7050
+    elif [ $ORG -eq 2 ]; then
+        ORDERER=$ORDERER1_IP:7050
+    fi
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+		peer chaincode upgrade -o $ORDERER -C $CHANNEL_NAME -n $CHAINCODE_NAME -v $VERSION  -l ${LANGUAGE} -p ${CC_SRC_PATH} -c  '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')" >&log.txt
+    else
+		peer chaincode upgrade -o $ORDERER --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME -v $VERSION  -l ${LANGUAGE} -p ${CC_SRC_PATH} -c '{"Args":["init"]}' -P "OR	('Org1MSP.member','Org2MSP.member')"  >&log.txt
+	fi
+	res=$?
+	cat log.txt
+	verifyResult $res "Chaincode upgradation on PEER$PEER on channel '$CHANNEL_NAME' failed"
+	echo "===================== Chaincode Upgradation on PEER$PEER on channel '$CHANNEL_NAME' ===================== "
+	echo
+}
+
+chaincodeInvoke () {
+	PEER=$1
+    ORG=$2
+    setGlobals $PEER $ORG
+    DATE_WITH_TIME=`date "+%Y-%m-%d %H:%M:%S.%3N"`
+    echo "Started time: " $DATE_WITH_TIME
+
+	local ORDERER=
+    if [ $ORG -eq 1 ]; then
+        ORDERER=$ORDERER0_IP:7050
+    elif [ $ORG -eq 2 ]; then
+        ORDERER=$ORDERER1_IP:7050
+    fi
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+		peer chaincode invoke -o $ORDERER -C $CHANNEL_NAME -n $CHAINCODE_NAME1 -c "'$CONSTRUCTOR'"  >&log.txt
+        echo "peer chaincode invoke -o" $ORDERER "-C" $CHANNEL_NAME "-n" $CHAINCODE_NAME1 "-c" "'$CONSTRUCTOR'"
+	else
+		peer chaincode invoke -o $ORDERER  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME1 -c $CONSTRUCTOR >&log.txt
+        echo "peer chaincode invoke -o" $ORDERER  "--tls" $CORE_PEER_TLS_ENABLED "--cafile" $ORDERER_CA "-C" $CHANNEL_NAME "-n" $CHAINCODE_NAME1 "-c" $CONSTRUCTOR
+	fi
+	res=$?
+	cat log.txt
+	verifyResult $res "Invoke execution on PEER$PEER failed "
+	echo "===================== Invoke transaction on PEER$PEER on channel '$CHANNEL_NAME' ===================== "
+	echo
+    echo "End time: " $DATE_WITH_TIME
+}
+
+chaincodeQuery () {
+    PEER=$1
+    ORG=$2
+    setGlobals $PEER $ORG
+
+    echo "===================== Querying on PEER$PEER on channel '$CHANNEL_NAME'... ===================== "
+
+    peer chaincode query -C $CHANNEL_NAME -n $CHAINCODE_NAME1 -c $CONSTRUCTOR >&log.txt
+    echo
+    cat log.txt
+}
+
+
+
+CHAINCODE_NAME1=mycc1
+CHAINCODE_NAME2=mycc2
+
+VERSION=0.1
+
+#installChaincode 0 1 $VERSION
+
+#installChaincode 0 2 $VERSION
+
+instantiateChaincode 0 1 $VERSION
+
+sleep 3
+
+
